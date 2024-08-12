ab34e5b0ae5ebd6ecc9f45c1b7375fe61df97aec
Trueman <23735771.tru@student.iuh.edu.vn>
2024-08-12 19:47:41 +0700

    Hello Bui Quoc Tru! Welcome to the DHKTPM19ATT - 422000411904!

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Bui Quoc Tru!");
+        System.out.println("Welcome to the DHKTPM19ATT - 422000411904!");
+    }
+}
